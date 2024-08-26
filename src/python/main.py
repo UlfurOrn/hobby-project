567af78ac9c074533378511e5cd4f41268cd5cd6
ulfur <ulfur@controlant.com>
2024-08-26 11:12:37 +0000

    Got a working build with packages from poetry

--- src/python/main.py
+++ src/python/main.py
@@ -1,6 +1,7 @@
-def hello_world():
-    print("Hello World")
+import uvicorn
+from fastapi import FastAPI
 
+app = FastAPI()
 
 if __name__ == "__main__":
-    hello_world()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
