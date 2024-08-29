558cbf17b5cfc1530a10a24bb5c5ff4108043171
ulfur <ulfur@controlant.com>
2024-08-29 13:49:30 +0000

    Stop using pants for the time being

--- src/pokemon/main.py
+++ src/pokemon/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(host="127.0.0.1", port=8000)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app)
