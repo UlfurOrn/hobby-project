7f6499f83c0d3b7081abfceb5012f1f9dea8cbc2
ulfur <ulfur@controlant.com>
2024-09-22 20:46:08 +0000

    Initialize a database connection on startup

--- src/pokemon_api/main.py
+++ src/pokemon_api/main.py
@@ -1,7 +1,27 @@
-import uvicorn
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI(host="127.0.0.1", port=8000)
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from shared.database.database import close_database, init_database
+
+from pokemon_api.config import ServiceConfig
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    load_dotenv()
+
+    config = ServiceConfig()
+
+    init_database(config.database)
+
+    yield
+
+    await close_database()
+
+
+app = FastAPI(host="127.0.0.1", port=8000, title="Pokemon API", version="0.0.0", lifespan=lifespan)
 
 if __name__ == "__main__":
     uvicorn.run(app)
