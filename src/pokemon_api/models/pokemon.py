8253d51ef9fa82d509091f77abe3d54ac45fc1e2
ulfur <ulfur@controlant.com>
2024-09-22 21:46:59 +0000

    Inital implementation of database model

---
+++ src/pokemon_api/models/pokemon.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class BaseDB(DeclarativeBase):
+    pass
+
+
+class PokemonDB(BaseDB):
+    __tablename__ = "pokemon"
+
+    name: Mapped[str] = mapped_column(primary_key=True)
+
+    health: Mapped[int]
+    attack: Mapped[int]
+    defence: Mapped[int]
+    special_attack: Mapped[int]
+    special_defence: Mapped[int]
+    speed: Mapped[int]
