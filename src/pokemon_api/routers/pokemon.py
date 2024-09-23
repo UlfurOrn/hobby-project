09e9cd6c1c946a4e6dc95b8165d9715c4939d6d0
ulfur <ulfur@controlant.com>
2024-09-23 18:10:54 +0000

    Prototype of endpoint working

--- src/pokemon_api/routers/pokemon.py
+++ src/pokemon_api/routers/pokemon.py
@@ -1,8 +1,37 @@
-from fastapi import APIRouter
+from typing import Annotated, Iterable
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from shared.database.database import get_db_session
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from pokemon_api.models.pokemon import PokemonDB
 
 router = APIRouter()
 
 
-@router.get("")
-def get_pokemon():
-    return ["Charizard"]
+async def managed_db_session():
+    async with get_db_session() as db_session:
+        yield db_session
+
+
+class Pokemon(BaseModel):
+    name: str
+
+    health: int
+    attack: int
+    defence: int
+    special_attack: int
+    special_defence: int
+    speed: int
+
+    total: int
+
+
+@router.get("", response_model=list[Pokemon])
+async def get_pokemon(db_session: Annotated[AsyncSession, Depends(managed_db_session)]) -> Iterable[PokemonDB]:
+    query = select(PokemonDB)
+    result = await db_session.scalars(query)
+
+    return result.all()
