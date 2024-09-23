29b3b6531f470c191c6eec0f52c2f6aec7fac674
ulfur <ulfur@controlant.com>
2024-09-23 15:41:46 +0000

    Alembic now working

--- src/pokemon_api/alembic/env.py
+++ src/pokemon_api/alembic/env.py
@@ -1,29 +1,18 @@
-from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+import asyncio
 
 from alembic import context
+from dotenv import load_dotenv
+from pokemon_api.config import DatabaseConfig
+from pokemon_api.models.pokemon import BaseDB
+from shared.database.database import close_database, get_db_engine, init_database
+from sqlalchemy import NullPool
+from sqlalchemy.engine import Connection
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# this is the Alembic Config object, which provides access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = BaseDB.metadata
 
 
 def run_migrations_offline() -> None:
@@ -38,9 +27,11 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    load_dotenv()
+    database_config = DatabaseConfig()
+
     context.configure(
-        url=url,
+        url=database_config.url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -50,26 +41,36 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
 
-    In this scenario we need to create an Engine
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    load_dotenv()
+    database_config = DatabaseConfig()
 
-        with context.begin_transaction():
-            context.run_migrations()
+    init_database(database_config, poolclass=NullPool, logging_name="alembic")
+
+    engine = get_db_engine()
+
+    async with engine.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await close_database()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
--- src/pokemon_api/alembic/script.py.mako
+++ src/pokemon_api/alembic/script.py.mako
@@ -3,12 +3,12 @@
 Revision ID: ${up_revision}
 Revises: ${down_revision | comma,n}
 Create Date: ${create_date}
-
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 ${imports if imports else ""}
 
 # revision identifiers, used by Alembic.
