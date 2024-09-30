fe399f3eb2604b895779d78c6f99cbccfddb6afa
ulfur <ulfur@controlant.com>
2024-09-30 21:06:37 +0000

    Added fixtures for API management

--- tests/pokemon_api/conftest.py
+++ tests/pokemon_api/conftest.py
@@ -1,9 +1,14 @@
 import pytest
+from fastapi import FastAPI
+from httpx import AsyncClient
 from pokemon_api.config import DatabaseConfig
+from pokemon_api.main import app
 from pokemon_api.models.pokemon import BaseDB
 from shared.database.database import close_database, get_db_engine, get_db_session, init_database
 from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
+
+### Database ###
 
 
 @pytest.fixture(scope="session")
@@ -52,3 +57,17 @@ async def clean_database(database):
 async def db_session(clean_database) -> AsyncSession:
     with get_db_session() as session:
         yield session
+
+
+### API ###
+
+
+@pytest.fixture
+def api(clean_database) -> FastAPI:
+    return app
+
+
+@pytest.fixture
+async def api_client(api) -> AsyncClient:
+    async with AsyncClient(app=api, base_url="http://testserver") as test_client:
+        yield test_client
